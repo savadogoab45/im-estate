91846ba51c6113bfdac3eb152c8736df780b5252
savadogo <savadogo.ab45@gmail.com>
2026-06-14 15:30:36 +0000

    j'ais crée les page et les routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import 'react'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Signin from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
